f88e6850926f09fe77310cb5f57342446b773ce7
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-12 18:15:07 +0400

    inheritence in java

--- FirstPrograms/src/Garage.java
+++
@@ -1,6 +0,0 @@
-
-public class Garage {
-    void park(Car car) {
-    	System.out.println("The " + car.name + " is parked");
-    }
-}
--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
         
-         Garage garage = new Garage();
-         Car car = new Car("bmw");
-         Car car2 = new Car("lamborghini");
-         
-         garage.park(car);
-         garage.park(car2);
-   
+      Dog dog = new Dog();
+      
+      dog.speak();
+      
 	}
 
 }
+
+
+
+
