e9c57f3ac11c1292d479f4c36b2285b2f4e1753f
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-12 16:59:49 +0400

    objects and constructors in java
    Change-Id: I7bdfbe7d2bc7dd87141dbf6c43ff03263c600956

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -3,9 +3,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-       final int number = 12;
-       
-       
+        
+        //Human human = new Human("luka", 23, 80);
+        Human human2 = new Human("giorgi", 43, 54);
+        
+        System.out.println(human2.age);
 	}
 
 }
