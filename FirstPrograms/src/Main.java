87ea6ecac73744bdded5606d892f8c29d023e8ab
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-12 16:15:54 +0400

    overloaded methods in java

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -3,14 +3,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-    
-	    int z = add(5, 100);
-	    System.out.println(z);
+      int x = add(1, 2, 10);
+	   System.out.println(x);
 	}
 	
-	 static int add(int x, int y) {
-		  int z = x + y;
-		  return z;
-	 }
-
+	// overloaded methods
+	
+     static int add(int a, int b) {
+    	 return a + b;
+     }
+     
+ 	
+     static int add(int a, int b, int c) {
+    	 return a + b + c;
+     }
 }
