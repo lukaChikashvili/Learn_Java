a43ea98f1dee26a8218dc489bed1b188f44ea0ca
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-12 18:39:47 +0400

    commit

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
         
-      Hero hero1 = new Hero("batman", 42, "$$$");
       
-      System.out.println(hero1.name);
-      System.out.println(hero1.age);
       
 	}
 
