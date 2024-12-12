5163ac9cc02faedd7ffdf207ed26f28b4c45b1ac
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-12 16:42:35 +0400

    add final keyword
    Change-Id: I1109206c8bb7735d8c2a8fd7abb72bb37f48d4b4

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -3,18 +3,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-      int x = add(1, 2, 10);
-	   System.out.println(x);
+       final int number = 12;
+       
+       
 	}
-	
-	// overloaded methods
-	
-     static int add(int a, int b) {
-    	 return a + b;
-     }
-     
- 	
-     static int add(int a, int b, int c) {
-    	 return a + b + c;
-     }
+
 }
