9e1a59771bb676b6506db4ca28d339e9763bcdb4
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 21:32:30 +0400

    how to read, file methods and delete file

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -8,6 +8,10 @@ public class Main {
 	    
 	    if(file.exists()) {
 	    	System.out.println("this file exists");
+	    	System.out.println(file.getPath()); // gets file path
+	    	System.out.println(file.getAbsolutePath()); // gets absolute path
+	    	file.delete();  // delete file
+	    	
 	    }else {
 	    	System.out.println("this file doesn't exists");
 	    }
