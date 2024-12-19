420a86e2bbeab7396e7ffc530bd5efadeef19243
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 21:46:47 +0400

    read files in java

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -1,20 +1,31 @@
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		try {
-			 FileWriter writer = new FileWriter("poem.txt");
-			 writer.write("this is a poem");
-			 writer.close();
+	try {
+		 FileReader reader = new FileReader("text.txt");
+		 int data = reader.read();
+		 while(data != -1) {
+			 System.out.print((char)data);
+			 data = reader.read();
 			 
-			   
-		}catch(IOException e ) {
-			 e.printStackTrace();
-		}
-	  
+			 
+		 }
+		 reader.close();
+		 
+		  
+		  
+	}catch(FileNotFoundException e) {
+		e.printStackTrace();
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+	 
 	   
 	   
 	   
