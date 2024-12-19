1e73f4cf0d83d8187bd3a9d592307433951a790c
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 21:58:58 +0400

    create gui in java

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -1,32 +1,24 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.JFrame;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+
 
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-	try {
-		 FileReader reader = new FileReader("text.txt");
-		 int data = reader.read();
-		 while(data != -1) {
-			 System.out.print((char)data);
-			 data = reader.read();
-			 
-			 
-		 }
-		 reader.close();
-		 
-		  
-		  
-	}catch(FileNotFoundException e) {
-		e.printStackTrace();
-	}catch(IOException e) {
-		e.printStackTrace();
-	}
-	 
+       JFrame frame = new JFrame();  // create a frame
+       
+       frame.setTitle("my first program");  // give title
+       frame.setSize(400,400);  //  size - x and y
+	   frame.setVisible(true);  // make frame visible
 	   
+	   ImageIcon image = new ImageIcon("image1_0.jpg"); // create image icon
+	   frame.setIconImage(image.getImage());  // change icon to image
+	   frame.getContentPane().setBackground(new Color(67, 255, 123)); // change background color
 	   
 	   
       
