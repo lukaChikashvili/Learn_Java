239173b4d37a216a5f5ed89893e295564634efe4
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 21:21:47 +0400

    exceptions in java
    Change-Id: Id5f2cbb716e9200a3ed394179de51db5c3b8d049

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -1,9 +1,30 @@
-
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("enter the whole number to divide by:");
+			int x = scanner.nextInt();
+			
+			System.out.println("enter the whole number to divide by:");
+			int y = scanner.nextInt();
+			
+			int z = x/y;
+			
+			System.out.println("result " + z);
+			
+			scanner.close();
+		}catch(ArithmeticException e) {
+			 System.out.println("you can't divide by 0");
+			 
+		}
         
+	
+		
+		
       
       
 	}
