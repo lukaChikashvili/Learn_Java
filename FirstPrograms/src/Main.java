e73d178b1a7943284c56d4db2e8828d4e968f835
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 22:14:16 +0400

    panels in java swing

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -1,28 +1,30 @@
+
 import java.awt.Color;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-
-
+import javax.swing.JPanel;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-	   JLabel label = new JLabel();	
-	   label.setText("what the hell is this?");
-	   label.setForeground(new Color(0x00FF00)); // text color
-	   label.setBackground(Color.black);
-	   
-	   
-	
-       JFrame frame = new JFrame();
-       
-       frame.setSize(500, 500);
-       frame.setVisible(true);
-       frame.add(label);
+		JPanel redPanel = new JPanel();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(0, 0, 250, 250);
+		
+		JPanel bluePanel = new JPanel();
+		bluePanel.setBackground(Color.blue);
+		bluePanel.setBounds(250, 0, 250, 250);
+		
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setLayout(null);
+      frame.setSize(750, 750);
+      frame.setVisible(true);
+      frame.add(redPanel);
+      frame.add(bluePanel);
+      
 	   
       
 	}
