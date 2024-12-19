bd8d2972e98b95c4e82450d9f88b48f2846f6841
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-19 21:28:29 +0400

    check if file exists in java

--- FirstPrograms/src/Main.java
+++ FirstPrograms/src/Main.java
@@ -1,30 +1,16 @@
-import java.util.Scanner;
+import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			Scanner scanner = new Scanner(System.in);
-			
-			System.out.println("enter the whole number to divide by:");
-			int x = scanner.nextInt();
-			
-			System.out.println("enter the whole number to divide by:");
-			int y = scanner.nextInt();
-			
-			int z = x/y;
-			
-			System.out.println("result " + z);
-			
-			scanner.close();
-		}catch(ArithmeticException e) {
-			 System.out.println("you can't divide by 0");
-			 
-		}
-        
 	
-		
-		
+	    File file = new File("text.txt");
+	    
+	    if(file.exists()) {
+	    	System.out.println("this file exists");
+	    }else {
+	    	System.out.println("this file doesn't exists");
+	    }
       
       
 	}
